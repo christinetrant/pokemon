b733cf2f1c5666ed002f7bbf18d5046b654be38f
Christine Trant <christinetrant@gmail.com>
2020-06-18 14:06:46 +0100

    Updates

---
+++ precache-manifest.26f5b669129dc183d5b6a2851bd368bf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d1973d859d8db9d3025eb855e4e9a52",
+    "url": "/pokemon/index.html"
+  },
+  {
+    "revision": "c77a56cfc1602d079ec8",
+    "url": "/pokemon/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "59a8a2014ad8810278f0",
+    "url": "/pokemon/static/css/main.9b82db1f.chunk.css"
+  },
+  {
+    "revision": "c77a56cfc1602d079ec8",
+    "url": "/pokemon/static/js/2.2023a9f2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokemon/static/js/2.2023a9f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59a8a2014ad8810278f0",
+    "url": "/pokemon/static/js/main.b7c22491.chunk.js"
+  },
+  {
+    "revision": "cdccaf97c266dc053410",
+    "url": "/pokemon/static/js/runtime-main.4b0b2e80.js"
+  },
+  {
+    "revision": "36d26dbcd032a9d91d891c9f22dfdec6",
+    "url": "/pokemon/static/media/Pokemon Solid.36d26dbc.ttf"
+  }
+]);
